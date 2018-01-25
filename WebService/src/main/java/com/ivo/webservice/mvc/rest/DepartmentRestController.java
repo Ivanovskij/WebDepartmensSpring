1eaa10bc065398965b570db4f5a13ff13420fec0
io <webworkk@mail.ru>
2018-01-25 15:32:42 +0300

    add logging for web services

--- WebService/src/main/java/com/ivo/webservice/mvc/rest/DepartmentRestController.java
+++ WebService/src/main/java/com/ivo/webservice/mvc/rest/DepartmentRestController.java
@@ -4,17 +4,22 @@ package com.ivo.webservice.mvc.rest;
 import com.ivo.webservice.mvc.dao.DepartmentServiceDAO;
 import com.ivo.webservice.mvc.model.AvgSalary;
 import com.ivo.webservice.mvc.model.Department;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
 import java.util.List;
 
 @CrossOrigin(origins = { "http://localhost:8084" })
 @RestController
 @RequestMapping(value = "/departments")
 public class DepartmentRestController {
+
+    private static final Logger logger = LoggerFactory.getLogger(DepartmentRestController.class);
 
     @Autowired
     DepartmentServiceDAO departmentServiceDao;
@@ -23,12 +28,11 @@ public class DepartmentRestController {
     public ResponseEntity<List<Department>> list() {
         List<Department> departments = departmentServiceDao.list();
         if (departments.isEmpty()) {
-            //logger.debug("Employees does not exists");
+            logger.debug("Department does not exists");
             return new ResponseEntity<List<Department>>(HttpStatus.NO_CONTENT);
         }
-        //logger.debug("Found " + employees.size() + " Employees");
-        //logger.debug(employees);
-        //logger.debug(Arrays.toString(employees.toArray()));
+        logger.debug("Found " + departments.size() + " departments");
+        logger.debug(Arrays.toString(departments.toArray()));
         return new ResponseEntity<List<Department>>(departments, HttpStatus.OK);
     }
 
@@ -36,17 +40,17 @@ public class DepartmentRestController {
     public ResponseEntity<Department> read(@PathVariable(value = "departmentId") int departmentId) {
         Department department = departmentServiceDao.read(departmentId);
         if (department == null) {
-            //logger.debug("Employee with id " + id + " does not exists");
+            logger.debug("Department with id " + department + " does not exists");
             return new ResponseEntity<Department>(HttpStatus.NOT_FOUND);
         }
-        //logger.debug("Found Employee:: " + employee);
+        logger.debug("Found Department:: " + department);
         return new ResponseEntity<Department>(department, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/create", method = RequestMethod.POST, produces = "application/json")
     public ResponseEntity<Department> create(@RequestBody Department department) {
         departmentServiceDao.create(department);
-        //logger.debug("Added:: " + employee);
+        logger.debug("Added: " + department);
         return new ResponseEntity<Department>(department, HttpStatus.CREATED);
     }
 
@@ -54,10 +58,11 @@ public class DepartmentRestController {
     public ResponseEntity<Void> update(@RequestBody Department department) {
         Department existingDep = departmentServiceDao.read(department.getId());
         if (existingDep == null) {
-            //logger.debug("Employee with id " + department.getId() + " does not exists");
+            logger.debug("Department with id " + department.getId() + " does not exists");
             return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
         } else {
             departmentServiceDao.update(department);
+            logger.debug("Updated: " + department);
             return new ResponseEntity<Void>(HttpStatus.OK);
         }
     }
@@ -66,18 +71,25 @@ public class DepartmentRestController {
     public ResponseEntity<Void> delete(@PathVariable(value = "departmentId") int departmentId) {
         Department department = departmentServiceDao.read(departmentId);
         if (department == null) {
-            //logger.debug("Employee with id " + id + " does not exists");
+            logger.debug("Department with id " + department + " does not exists");
             return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
         } else {
             departmentServiceDao.delete(departmentId);
-            //logger.debug("Employee with id " + id + " deleted");
+            logger.debug("Department with id " + departmentId + " deleted");
             return new ResponseEntity<Void>(HttpStatus.OK);
         }
     }
 
     @RequestMapping(value = "/averageSalary", method = RequestMethod.GET, produces = "application/json")
-    public List<AvgSalary> averageSalary() {
-        return departmentServiceDao.averageSalaryByDepartment();
+    public ResponseEntity<List<AvgSalary>> averageSalary() {
+        List<AvgSalary> salaries = departmentServiceDao.averageSalaryByDepartment();
+        if (salaries.isEmpty()) {
+            logger.debug("Salaryies by department does not exists");
+            return new ResponseEntity<List<AvgSalary>>(HttpStatus.NO_CONTENT);
+        }
+        logger.debug("Found " + salaries.size() + " salaries by departments");
+        logger.debug(Arrays.toString(salaries.toArray()));
+        return new ResponseEntity<List<AvgSalary>>(salaries, HttpStatus.OK);
     }
 
 }
--- WebService/src/main/java/com/ivo/webservice/mvc/service/EmployeeServiceDAOImpl.java
+++ WebService/src/main/java/com/ivo/webservice/mvc/service/EmployeeServiceDAOImpl.java
@@ -1,23 +1,27 @@
 package com.ivo.webservice.mvc.service;
 
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.util.List;
-import javax.annotation.PostConstruct;
-import javax.sql.DataSource;
-
 import com.ivo.webservice.mvc.dao.EmployeeServiceDAO;
 import com.ivo.webservice.mvc.model.Employee;
 import com.ivo.webservice.mvc.model.EmployeeMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
 @Service
 public class EmployeeServiceDAOImpl implements EmployeeServiceDAO {
+
+    private static final Logger logger = LoggerFactory.getLogger(EmployeeServiceDAOImpl.class);
 
     @Autowired
     private DataSource dataSource;
@@ -32,6 +36,8 @@ public class EmployeeServiceDAOImpl implements EmployeeServiceDAO {
     }
 
     public boolean create(final Employee employee) {
+        logger.debug("called method create");
+
         String query = "insert into EMPLOYEES (FIO, DATE_OF_BIRTH, SALARY, NAME_DEP) values (?, ?, ?, ?)";
         this.jdbcTemplate.update(new PreparedStatementCreator() {
             public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
@@ -47,6 +53,8 @@ public class EmployeeServiceDAOImpl implements EmployeeServiceDAO {
     }
 
     public Employee read(int id) {
+        logger.debug("called method read");
+
         String query = "select * from EMPLOYEES where IDEMPLOYEES = ?";
 
         try {
@@ -58,25 +66,45 @@ public class EmployeeServiceDAOImpl implements EmployeeServiceDAO {
     }
 
     public List<Employee> list() {
+        logger.debug("called method list");
+
         String query = "select * from EMPLOYEES order by IDEMPLOYEES";
         List<Employee> employees = this.jdbcTemplate.query(query, new EmployeeMapper());
         return employees;
     }
 
     public boolean update(Employee employee) {
+        logger.debug("called method update");
+
         String query = "update EMPLOYEES set FIO = ?, DATE_OF_BIRTH = ?, SALARY = ?, NAME_DEP = ? where IDEMPLOYEES = ?";
         int result = this.jdbcTemplate.update("update EMPLOYEES set FIO = ?, DATE_OF_BIRTH = ?, SALARY = ?, NAME_DEP = ? where IDEMPLOYEES = ?", new Object[]{employee.getFio(), employee.getDateOfBirth(), employee.getSalary(), employee.getDepartment(), employee.getId()});
-        return result > 0;
+        if (result > 0) {
+            logger.debug("update success");
+            return true;
+        } else {
+            logger.debug("update error");
+            return false;
+        }
     }
 
     public boolean delete(int id) {
+        logger.debug("called method delete");
+
         String query = "delete from EMPLOYEES where IDEMPLOYEES like ?";
         int result = this.jdbcTemplate.update("delete from EMPLOYEES where IDEMPLOYEES like ?", new Object[]{id});
-        return result > 0;
+        if (result > 0) {
+            logger.debug("delete success");
+            return true;
+        } else {
+            logger.debug("delete error");
+            return false;
+        }
     }
 
     @Override
     public List<Employee> search(Date dateOfBirth) {
+        logger.debug("called method search");
+
         String query = "select * from EMPLOYEES where DATE_OF_BIRTH = ? order by IDEMPLOYEES";
 
         List<Employee> employees = this.jdbcTemplate.query(query, new Object[]{dateOfBirth}, new EmployeeMapper());
@@ -86,6 +114,8 @@ public class EmployeeServiceDAOImpl implements EmployeeServiceDAO {
 
     @Override
     public List<Employee> searchBetweenDates(Date from, Date to) {
+        logger.debug("called method searchBetweenDates");
+
         String query = "select * from EMPLOYEES where DATE_OF_BIRTH between ? and ? order by IDEMPLOYEES";
 
         List<Employee> employees = this.jdbcTemplate.query(query, new Object[]{from, to}, new EmployeeMapper());
